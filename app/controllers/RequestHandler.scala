120b9db7b7ae524f86704085110a9f0720b050a4
Michael Bryzek <mbryzek@alum.mit.edu>
2016-07-20 08:04:51 -0400

    Use DefaultHttpRequestHandler to implement play filters

--- app/controllers/RequestHandler.scala
+++ app/controllers/RequestHandler.scala
@@ -2,20 +2,36 @@ package controllers
 
 import javax.inject.Inject
 import lib.Constants
-import play.api.http.HttpRequestHandler
+import play.api.routing.SimpleRouter
+import play.api.http._
 import play.api.mvc._
+import scala.runtime.AbstractPartialFunction
 
 class RequestHandler @Inject() (
+  errorHandler: HttpErrorHandler,
+  configuration: HttpConfiguration,
+  filters: HttpFilters,
+  myRouter: Router
+) extends DefaultHttpRequestHandler(
+  myRouter, errorHandler, configuration, filters
+)
+
+
+class Router @Inject() (
   internal: Internal,
   proxy: ReverseProxy
-) extends HttpRequestHandler {
+) extends SimpleRouter {
 
-  def handlerForRequest(request: RequestHeader) = {
-    (request.method, request.path, request.headers.get(Constants.Headers.FlowService)) match {
-      case ("GET", "/_internal_/healthcheck", None) => (request, internal.getHealthcheck)
-      case ("GET", "/_internal_/config", None) => (request, internal.getConfig)
-      case _ => (request, proxy.handle)
+  override def routes = new AbstractPartialFunction[RequestHeader, Handler] {
+    override def applyOrElse[A <: RequestHeader, B >: Handler](request: A, default: A => B) = {
+      (request.method, request.path, request.headers.get(Constants.Headers.FlowService)) match {
+        case ("GET", "/_internal_/healthcheck", None) => internal.getHealthcheck
+        case ("GET", "/_internal_/config", None) => internal.getConfig
+        case _ => proxy.handle
+      }
     }
+
+    def isDefinedAt(rh: RequestHeader) = true
   }
 
 }
