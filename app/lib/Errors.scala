f6a8cb13ccd76919542319ca207b92f0076ed0a6
Michael Bryzek <mbryzek@alum.mit.edu>
2016-10-07 21:48:17 -0400

    Add global error handler

--- app/lib/Errors.scala
+++ app/lib/Errors.scala
@@ -3,8 +3,13 @@ package lib
 import io.flow.error.v0.models.GenericError
 import io.flow.error.v0.models.json._
 import play.api.libs.json.{Json, JsValue}
+import java.util.UUID
 
 trait Errors {
+
+  def generateErrorId(): String = {
+    "err-" + UUID.randomUUID.toString.replaceAll("-", "")
+  }
 
   def genericError(message: String): JsValue = {
     genericErrors(Seq(message))
@@ -15,6 +20,20 @@ trait Errors {
       GenericError(
         messages = messages
       )
+    )
+  }
+
+  def clientErrors(messages: Seq[String]): JsValue = {
+    Json.obj(
+      "code" -> "client_error",
+      "messages" -> messages
+    )
+  }
+
+  def serverErrors(messages: Seq[String]): JsValue = {
+    Json.obj(
+      "code" -> "server_error",
+      "messages" -> messages
     )
   }
 
