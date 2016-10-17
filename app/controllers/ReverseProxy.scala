169c2adae1c4dd0b241ea09dfbc0b102489988ef
Michael Bryzek <mbryzek@alum.mit.edu>
2016-10-17 08:48:26 -0400

    Set environment from the org auth data

--- app/controllers/ReverseProxy.scala
+++ app/controllers/ReverseProxy.scala
@@ -352,6 +352,7 @@ class ReverseProxy @Inject () (
       Some(
         token.copy(
           organizationId = Some(organization),
+          environment = Some(orgAuth.environment.toString),
           role = Some(orgAuth.role.toString)
         )
       )
