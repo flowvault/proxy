75b4b81719faa252e8beac001e65c1096e5a228a
Michael Bryzek <mbryzek@alum.mit.edu>
2017-03-03 11:19:46 -0500

    Make more reliable

--- scripts/test.rb
+++ scripts/test.rb
@@ -15,24 +15,24 @@ end
 puts "logging to %s" % ProxyGlobal::LOG_FILE
 puts ""
 
-TEST_ORG_PREFIX = "prefix-test"
+PARENT_ORGANIZATION_ID = "flow"
+TEST_ORG_PREFIX = "proxy-test"
 
 ## Deletes organizations with a given name prefix.
 ## Does not currently paginate
-def delete_test_orgs(helpers, prefix)
-  helpers.get("/organizations?limit=100").with_api_key.execute.json.each do |r|
-    if r['id'].start_with?(prefix) && r['environment'] == "sandbox"
+def delete_test_orgs(helpers, parent, prefix)
+  helpers.get("/organizations?limit=100&environment=sandbox&parent=#{parent}").with_api_key.execute.json.each do |r|
+    if r['id'].start_with?(prefix)
       assert_statuses([204, 404], helpers.delete("/organizations/#{r['id']}").with_api_key.execute)
     end
   end
 end
 
 def cleanup(helpers)
-  delete_test_orgs(helpers, TEST_ORG_PREFIX)
+  delete_test_orgs(helpers, PARENT_ORGANIZATION_ID, TEST_ORG_PREFIX)
 end
 
 helpers = Helpers.new("http://localhost:7000", api_key_file)
-cleanup(helpers)
 
 response = helpers.json_post("/foo").execute
 assert_generic_error(response, "Unknown HTTP path /foo")
@@ -56,7 +56,7 @@ assert_unauthorized(response)
 id = "%s-%s" % [TEST_ORG_PREFIX, ProxyGlobal.random_string(8)]
 response = helpers.json_post("/organizations", { :environment => 'sandbox', :parent_id => 'flow', "id" => id }).with_api_key.execute
 assert_status(201, response)
-assert_equals(response.json['id'], id + "1")
+assert_equals(response.json['id'], id)
 
 cleanup(helpers)
 
