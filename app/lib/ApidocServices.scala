4b13562ff9ad2fd0ceb1fb6e2fd1822be2b89b06
Michael Bryzek <mbryzek@alum.mit.edu>
2017-07-16 08:10:44 -0400

    Rename apidoc to apibuilder

--- app/lib/ApidocServices.scala
+++ app/lib/ApidocServices.scala
@@ -10,11 +10,11 @@ import play.api.Logger
   * apidoc.service.uris.
   */
 @Singleton
-class ApidocServicesFetcher @Inject() (
+class ApiBuilderServicesFetcher @Inject() (
   config: Config
 ) {
 
-  private[this] lazy val Uris: Seq[String] = config.requiredString("apidoc.service.uris").split(",").map(_.trim)
+  private[this] lazy val Uris: Seq[String] = config.requiredString("apibuilder.service.uris").split(",").map(_.trim)
 
   def load(urls: Seq[String]): Either[Seq[String], MultiService] = {
     Logger.info(s"ApidocServicesFetcher: fetching configuration from uris[${Uris.mkString(", ")}]")
