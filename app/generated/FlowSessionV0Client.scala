dbd91f98c30e2bc4efb226fe628af1bec3a169e5
Michael Bryzek <mbryzek@alum.mit.edu>
2017-07-16 09:10:32 -0400

    Manually remove experience geo from session to avoid pulling in experience

--- app/generated/FlowSessionV0Client.scala
+++ app/generated/FlowSessionV0Client.scala
@@ -66,8 +66,7 @@ package io.flow.session.v0.models {
     country: io.flow.reference.v0.models.Country,
     currency: io.flow.reference.v0.models.Currency,
     language: io.flow.reference.v0.models.Language,
-    locale: io.flow.reference.v0.models.Locale,
-    experience: io.flow.experience.v0.models.ExperienceGeo
+    locale: io.flow.reference.v0.models.Locale
   )
 
   /**
@@ -253,7 +252,6 @@ package io.flow.session.v0.models {
     import play.api.libs.functional.syntax._
     import io.flow.common.v0.models.json._
     import io.flow.error.v0.models.json._
-    import io.flow.experience.v0.models.json._
     import io.flow.reference.v0.models.json._
     import io.flow.session.v0.models.json._
 
@@ -299,8 +297,7 @@ package io.flow.session.v0.models {
         (__ \ "country").read[io.flow.reference.v0.models.Country] and
         (__ \ "currency").read[io.flow.reference.v0.models.Currency] and
         (__ \ "language").read[io.flow.reference.v0.models.Language] and
-        (__ \ "locale").read[io.flow.reference.v0.models.Locale] and
-        (__ \ "experience").read[io.flow.experience.v0.models.ExperienceGeo]
+        (__ \ "locale").read[io.flow.reference.v0.models.Locale]
       )(LocalSession.apply _)
     }
 
@@ -309,8 +306,7 @@ package io.flow.session.v0.models {
         "country" -> io.flow.reference.v0.models.json.jsObjectCountry(obj.country),
         "currency" -> io.flow.reference.v0.models.json.jsObjectCurrency(obj.currency),
         "language" -> io.flow.reference.v0.models.json.jsObjectLanguage(obj.language),
-        "locale" -> io.flow.reference.v0.models.json.jsObjectLocale(obj.locale),
-        "experience" -> io.flow.experience.v0.models.json.jsObjectExperienceGeo(obj.experience)
+        "locale" -> io.flow.reference.v0.models.json.jsObjectLocale(obj.locale)
       )
     }
 
@@ -702,7 +698,6 @@ package io.flow.session.v0 {
   ) extends interfaces.Client {
     import io.flow.common.v0.models.json._
     import io.flow.error.v0.models.json._
-    import io.flow.experience.v0.models.json._
     import io.flow.reference.v0.models.json._
     import io.flow.session.v0.models.json._
 
@@ -951,7 +946,6 @@ package io.flow.session.v0 {
 
     import io.flow.common.v0.models.json._
     import io.flow.error.v0.models.json._
-    import io.flow.experience.v0.models.json._
     import io.flow.reference.v0.models.json._
     import io.flow.session.v0.models.json._
 
--- app/lib/ApiBuilderServicesFetcher.scala
+++ app/lib/ApiBuilderServicesFetcher.scala
@@ -5,9 +5,9 @@ import javax.inject.{Inject, Singleton}
 import play.api.Logger
 
 /**
-  * Responsible for downloading the apidoc service specifications from
-  * the URLs specified by the configuration parameter named
-  * apidoc.service.uris.
+  * Responsible for downloading the API Builder service specifications
+  * from the URLs specified by the configuration parameter named
+  * apibuilder.service.uris
   */
 @Singleton
 class ApiBuilderServicesFetcher @Inject() (
@@ -17,12 +17,12 @@ class ApiBuilderServicesFetcher @Inject() (
   private[this] lazy val Uris: Seq[String] = config.requiredString("apibuilder.service.uris").split(",").map(_.trim)
 
   def load(urls: Seq[String]): Either[Seq[String], MultiService] = {
-    Logger.info(s"ApidocServicesFetcher: fetching configuration from uris[${Uris.mkString(", ")}]")
+    Logger.info(s"ApiBuilderServicesFetcher: fetching configuration from uris[${Uris.mkString(", ")}]")
     MultiService.fromUrls(urls)
   }
 
   def current(): MultiService = load(Uris) match {
-    case Left(errors) => sys.error(s"Error loading apidoc services from uris[$Uris]: $errors")
+    case Left(errors) => sys.error(s"Error loading API Builder services from uris[$Uris]: $errors")
     case Right(multi) => multi
   }
 
