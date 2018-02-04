834ea7a2334adc0ad4b6f3345d87175cfb4bcfe7
Michael Bryzek <mike@flow.io>
2018-02-04 09:38:40 -0500

    Errors

--- app/controllers/ReverseProxy.scala
+++ app/controllers/ReverseProxy.scala
@@ -61,7 +61,7 @@ class ReverseProxy @Inject () (
       if (all.isDefinedAt(s.name)) {
         sys.error(s"Duplicate server with name[${s.name}]")
       } else {
-        all += (s.name -> serverProxyFactory(ServerProxyDefinition(s)))
+        all += (s.name -> serverProxyFactory(ServerProxyDefinition(s, multiService)))
       }
     }
     all.toMap
