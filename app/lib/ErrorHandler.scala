f120696c55f3348a9f22412998f2dccf49efaa0b
Paolo Lim <paolo@flow.io>
2018-11-06 13:39:30 -0500

    Refactor ErrorHandler to use server.logger

--- app/lib/ErrorHandler.scala
+++ app/lib/ErrorHandler.scala
@@ -10,49 +10,48 @@ import javax.inject.{Inject, Singleton}
 
 @Singleton
 class ErrorHandler @Inject() (
-  logger: RollbarLogger,
-  proxyConfigFetcher: ProxyConfigFetcher
+  proxyConfigFetcher: ProxyConfigFetcher,
+  defaultLogger: RollbarLogger
 ) extends HttpErrorHandler with Errors {
 
   lazy val index: Index = proxyConfigFetcher.current()
 
-  def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
-    val errorId = generateErrorId()
-    val operation = index.resolve(Method(request.method), request.uri)
+  private[this] case class Foo(errorId: String, logger: RollbarLogger)
 
-    logger.
-      withKeyValue("error_id", errorId).
-      withKeyValue("request_ip", request.remoteAddress).
-      withKeyValue("request_method", request.method.toString).
-      withKeyValue("request_uri", request.uri).
+  def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
+    val foo = rollbarLogger(request)
+    foo.logger.
       withKeyValue("http_status_code", statusCode).
       withKeyValue("message", message).
-      withKeyValue("route_path", operation.map(_.route.path)).
-      withKeyValue("route_server_name", operation.map(_.server.name)).
-      withKeyValue("route_server_host", operation.map(_.server.host)).
       warn("Client error")
 
-    Future.successful(
-      Status(statusCode)(clientErrors(Seq(s"Invalid request (err #$errorId)")))
-    )
+    val r = Status(statusCode)(clientErrors(Seq(s"Invalid request (err #${foo.errorId})")))
+    Future.successful(r)
   }
 
   def onServerError(request: RequestHeader, ex: Throwable): Future[Result] = {
-    val errorId = generateErrorId()
+    val foo = rollbarLogger(request)
+    foo.logger.error("FlowError", ex)
+
+    val r = InternalServerError(serverErrors(Seq(s"A server error occurred (err #${foo.errorId})")))
+    Future.successful(r)
+  }
+
+  private[this] def rollbarLogger(request: RequestHeader): Foo = {
     val operation = index.resolve(Method(request.method), request.uri)
+    val errorId = generateErrorId()
+    val baseLogger = operation.map(_.server.logger).getOrElse(defaultLogger)
 
-    logger.
-      withKeyValue("error_id", errorId).
-      withKeyValue("request_ip", request.remoteAddress).
-      withKeyValue("request_method", request.method.toString).
-      withKeyValue("request_uri", request.uri).
-      withKeyValue("route_path", operation.map(_.route.path)).
-      withKeyValue("route_server_name", operation.map(_.server.name)).
-      withKeyValue("route_server_host", operation.map(_.server.host)).
-      error("FlowError", ex)
-
-    Future.successful(
-      InternalServerError(serverErrors(Seq(s"A server error occurred (err #$errorId)")))
+    Foo(
+      errorId,
+      baseLogger.
+        withKeyValue("error_id", errorId).
+        withKeyValue("request_ip", request.remoteAddress).
+        withKeyValue("request_method", request.method.toString).
+        withKeyValue("request_uri", request.uri).
+        withKeyValue("route_path", operation.map(_.route.path)).
+        withKeyValue("route_server_name", operation.map(_.server.name)).
+        withKeyValue("route_server_host", operation.map(_.server.host))
     )
   }
 }
