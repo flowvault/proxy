6a153a7cc7ddac4b0aa667880b99a64c9478b69b
Jean Logeart <jean.logeart@gmail.com>
2019-12-27 12:05:51 -0500

    Remove unnecessary

--- app/lib/Util.scala
+++ app/lib/Util.scala
@@ -2,22 +2,9 @@ package lib
 
 object Util {
 
-  def toFlatSeq(data: Map[String, Seq[String]]): Seq[(String, String)] = {
+  def toFlatSeq(data: Map[String, Seq[String]]): Seq[(String, String)] =
     data.map { case (k, vs) =>
       vs.map(k -> _)
     }.flatten.toSeq
-  }
-
-  def removeKeys(
-    data: Map[String, Seq[String]],
-    keys: Set[String],
-  ): Map[String, Seq[String]] =
-    data -- keys
-
-  def filterKeys(
-    data: Map[String, Seq[String]],
-    keys: Set[String]
-  ): Map[String, Seq[String]] =
-    data.filterKeys(keys)
 
 }
