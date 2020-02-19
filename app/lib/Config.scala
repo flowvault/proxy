cf2c48c0ff451820ab405c9da17f35abf8071cf7
Juwen <juwen@flow.io>
2020-02-19 11:14:22 -0500

    simplify

--- app/lib/Config.scala
+++ app/lib/Config.scala
@@ -20,7 +20,7 @@ class Config @Inject() (
 
   private[this] lazy val VerboseLogPrefixes: Seq[String] = optionalString(Names.VerboseLogPrefixes).getOrElse("").
     split(",").
-    toIndexedSeq.
+    toSeq.
     map(_.trim).
     filterNot(_.isEmpty)
 
